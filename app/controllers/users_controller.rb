d85cd6dacd7669eec2ade634a515696ef555137a
OmarMWarraich <earthling986@gmail.com>
2024-04-02 02:25:56 +0500

    feat: View Stocks, Refine Nav

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
   def my_portfolio
-    
+    @tracked_stocks = current_user.stocks
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path, notice: 'User was successfully destroyed.'
   end
 end
