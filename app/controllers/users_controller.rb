51190392227a1943caadd29a2b05292051656706
OmarMWarraich <earthling986@gmail.com>
2024-04-03 21:20:17 +0500

    feat: Friends Search Turbo Submission/Response

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,30 @@ class UsersController < ApplicationController
   end
 
   def search
-    render json: params[:friend]
+    if params[:friend].present?
+      @friend = params[:friend]
+      if @friend
+        respond_to do |format|
+          format.turbo_stream do
+            render turbo_stream: turbo_stream.append("friend-result", partial: "users/friend_result", locals: { friend: @friend }) +
+                                 turbo_stream.replace("friends-search-form", partial: "users/friends_search_form")                                  
+          end
+        end
+      else
+        flash[:alert] = "Could not find user"
+        respond_to do |format|
+          format.turbo_stream do
+            render turbo_stream: turbo_stream.replace("flash", partial: "layouts/messages")
+          end
+        end
+      end
+    else
+      flash[:alert] = "Please enter a username to search"
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace("flash", partial: "layouts/messages")
+        end
+      end
+    end
   end
 end
