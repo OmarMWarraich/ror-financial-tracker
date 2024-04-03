5cf5d13db6aef9be4cd646b75f1c54fe2fa5d9d8
OmarMWarraich <earthling986@gmail.com>
2024-04-03 14:36:12 +0500

    feat: Restrict/Remove Stock Tracking

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -19,4 +19,20 @@ class UserStocksController < ApplicationController
       format.html { redirect_to my_portfolio_path }
     end
   end
+
+  def destroy
+    @tracked_stocks = current_user.stocks
+    stock = Stock.find(params[:id])
+    user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+    user_stock.destroy
+    flash.now[:alert] = "#{stock.ticker.upcase} was successfully removed from your portfolio"
+
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace("flash", partial: "layouts/messages") +
+                             turbo_stream.replace("stocks-list", partial: "stocks/list", locals: { tracked_stocks: @tracked_stocks }) 
+      end
+      format.html { redirect_to my_portfolio_path }
+    end
+end
 end
