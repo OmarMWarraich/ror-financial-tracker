f93bbfe790c4e5834e3258031817610731c70f2c
OmarMWarraich <earthling986@gmail.com>
2024-04-03 20:28:49 +0500

    feat: Search Friend Form

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
   get 'my_friends', to: 'users#my_friends'
+  get 'search_friend', to: 'users#search'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
